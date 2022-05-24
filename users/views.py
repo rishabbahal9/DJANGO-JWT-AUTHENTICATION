516efd07a72748771891f9f9f69184b7212643ac
Rishab Bahal <rishabbahal@gmail.com>
2022-05-24 01:34:27 -0400

    Proper validations added to user endpoint

--- users/views.py
+++ users/views.py
@@ -4,7 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
 from users.serializers import UserSerializer
 from .models import User
-
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
 
 class RegisterView(APIView):
     def post(self, request):
@@ -23,9 +24,14 @@ class TestView(APIView):
 
 
 class UserView(APIView):
+    permission_classes = (IsAuthenticated, )
+    
     def get(self, request):
         token = request.META['HTTP_AUTHORIZATION'].split(" ")[1]
-        access_token_obj = AccessToken(token)
-        user_id = access_token_obj['user_id']
+        try:
+            access_token_obj = AccessToken(token)
+            user_id = access_token_obj['user_id']
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Could not verify token\'s authenticity!')
         user = User.objects.filter(id=user_id).first()
         return Response({"id": user.id ,"first_name": user.first_name, "last_name": user.last_name, "username": user.username, "email": user.email})
