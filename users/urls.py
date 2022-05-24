d82486279b8d80199beb3cf5ea2755e6135ed529
Rishab Bahal <rishabbahal@gmail.com>
2022-05-24 01:23:03 -0400

    getting user details from jwt access token endpoint made

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
-from .views import RegisterView
+from .views import RegisterView, UserView
 
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
-
+    path('user', UserView.as_view()),
     path('token/',
          jwt_views.TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import AccessToken
 from users.serializers import UserSerializer
+from .models import User
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -9,9 +12,20 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
 class TestView(APIView):
     permission_classes = (IsAuthenticated, )
 
     def get(self, request):
         content = {'message': 'Test, Successful'}
         return Response(content)
+
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.META['HTTP_AUTHORIZATION'].split(" ")[1]
+        access_token_obj = AccessToken(token)
+        user_id = access_token_obj['user_id']
+        user = User.objects.filter(id=user_id).first()
+        return Response({"id": user.id ,"first_name": user.first_name, "last_name": user.last_name, "username": user.username, "email": user.email})
