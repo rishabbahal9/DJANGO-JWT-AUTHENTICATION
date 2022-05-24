938214d5eb2cd911c5480d7733df90a13e7d5e32
Rishab Bahal <rishabbahal@gmail.com>
2022-05-23 23:30:34 -0400

    Cleared git cache

--- users/views.py
+++ users/views.py
@@ -4,8 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class HelloView(APIView):
-	permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, )
 
-	def get(self, request):
-		content = {'message': 'Hello, GeeksforGeeks'}
-		return Response(content)
+    def get(self, request):
+        content = {'message': 'Hello, GeeksforGeeks'}
+        return Response(content)
